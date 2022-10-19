09c894e95822d0fb8465f74deda5659f8a055ef9
Guillaume Soulichanh <g.soulichanh@gmail.com>
2022-10-19 16:42:08 +0200

    Ajout des entites

--- gestiondestock/src/main/java/model/AbstractEntity.java
+++ gestiondestock/src/main/java/model/AbstractEntity.java
@@ -4,12 +4,14 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -17,6 +19,7 @@ import lombok.Data;
 
 @Data
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class AbstractEntity implements Serializable {
 	
 	@Id
